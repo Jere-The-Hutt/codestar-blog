40f7ba8633abfdc82eb40086930407d7b5aa4dc8
Jere-The-Hutt <jere.huttunen@live.com>
2025-05-17 16:18:52 +0200

    Create About app, model, views, template and urls

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-deo!l*kq3z^cn@nvfdcg-c(!ha(46g)uz$7p9^v3t#05#givme
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_summernote',
     'blog',
+    'about',
 ]
 
 MIDDLEWARE = [
