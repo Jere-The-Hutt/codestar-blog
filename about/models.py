b69fe6e22064ae0587f6ab69e3e170bd3f4048a7
Jere-The-Hutt <jere.huttunen@live.com>
2025-05-19 15:31:19 +0200

    Shows user generated image on post_detail.html

--- about/models.py
+++ about/models.py
@@ -9,6 +9,7 @@ class About(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     profile_image = CloudinaryField('image', default='placeholder')
+
     def __str__(self):
         return self.title
 
